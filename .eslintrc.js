b54e190ff22dbfa12691a434a51b325bedc7a477
周成成 <chengcheng.zhou@mushiny.com>
2022-05-09 09:48:29 +0800

    v0.0.1: 完善eslint、tsconfig

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "jest"],
-  extends: ["airbnb-typescript", "plugin:jest/recommended", "prettier"],
-};
